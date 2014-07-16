7e2bdb3609258e4d2b7230d2f1a39c259c71a90c
jpazdera <jpazdera@haverford.edu>
2014-07-16 18:39:00 -0400

    Minor Improvements
    Updated some help text. Removed potential issue in which duplicate
    contractors could be made through editing process. Adjusted text
    formatting on forms and search result lists.

--- Non-Html/data_modifiers.js
+++ Non-Html/data_modifiers.js
@@ -1,5 +1,5 @@
 function addContractor() { //adds a contractor using the form on the AddContractors.html page
-	//get info from form
+	//get info from form and create new contractor object
 	var name = document.getElementById("name").value;
 	var address = document.getElementById("address").value;
 	var city = document.getElementById("city").value;
@@ -9,25 +9,19 @@ function addContractor() { //adds a contractor using the form on the AddContract
 	var email = document.getElementById("email").value;
 	var contact = document.getElementById("contact").value;
 	var last = document.getElementById("last").value;
-	if (name == "" || zip == "" || last =="") {
-		alert("Error: Invalid input. Contractor name, zip code, and last use required.");
+	var contractor = {"Town":city,"Name":name,"Zip":zip,"Last Use":last,"Phone":phone,"State":state,"Contact":contact,"Address":address};
+		
+	//if invalid input, do not enter contractor
+	var a = validateEntry(name, zip, last);
+	var cont = a[0];
+	last = a[1];
+	if (!cont) { 
 		return;
 	}
-	//create new contractor object
-	var contractor = {"Town":city,"Name":name,"Zip":zip,"Last Use":last,"Phone":phone,"State":state,"Contact":contact,"Address":address};
+
 	//update database
-	if (zip in CONTRACTOR_INFO) {
-		//prevent the addition of duplicate entries
-		for (var i=0; i<CONTRACTOR_INFO[zip].length; i++) {
-			if (CONTRACTOR_INFO[zip][i]["Name"] == contractor["Name"]) {
-				alert("Error: Contractor \""+updated[zip][i]["Name"]+"\" already present in database. Duplicates cannot be added.");
-				return;
-			}
-		}
-		CONTRACTOR_INFO[zip].push(contractor);
-	} else {
-		CONTRACTOR_INFO[zip] = [contractor];
-	}
+	avoidDuplicatesAdd (zip, contractor);
+	
 	resetForm();
 }
 
@@ -76,12 +70,16 @@ function editContractor(id) { //makes edits to the
 	var last = document.getElementById("last"+id).value;
 	var contractor = {"Town":city,"Name":name,"Zip":zip,"Last Use":last,"Phone":phone,"State":state,"Contact":contact,"Address":address,"Email":email};
 	
-	//insert updated contractor where appropriate
-	if (zip in CONTRACTOR_INFO) {
-		CONTRACTOR_INFO[zip].push(contractor);
-	} else {
-		CONTRACTOR_INFO[zip] = [contractor];
+	//if invalid input, do not save updated info
+	var a = validateEntry(name, zip, last);
+	var cont = a[0];
+	last = a[1];
+	if (!cont) { 
+		return;
 	}
+	
+	//insert updated contractor where appropriate
+	avoidDuplicatesAdd (zip, contractor);
 }
 
 
@@ -119,4 +117,40 @@ function prefillForm(n, z, id) { //generates and pre-fills edit forms with exist
 		+'Contact: <input id="contact'+id+'" type="text" name="code" value=\"'+contact+'\" /><br>'
 		+'Last Use (Mmm-YY): <input id="last'+id+'" type="text" name="code" value=\"'+last+'\" /><br>'
 		+'</form>');
+}
+
+
+function validateEntry(name, zip, last) {
+	//require contractor name and zip code
+	if (name == "" || zip == "") {
+		alert("Error: Invalid input. Contractor name, zip code, and last use required.");
+		return [false, last];
+	}
+	//if no last use is entered, default to current month/year
+	if (last == "") {
+		var new_date = new Date();
+		var m = new_date.getMonth();
+		var y = new_date.getFullYear() - 2000; //will need to be changed in the year 2100
+		y = String(y);
+		var month_dict = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		last = month_dict[m];
+		last = last.concat("-");
+		last = last.concat(y);
+	}
+	return [true, last];
+}
+
+
+function avoidDuplicatesAdd (zip, contractor) { //prevent the addition of duplicate entries while adding new/updated contractor
+	if (zip in CONTRACTOR_INFO) {
+		for (var i=0; i<CONTRACTOR_INFO[zip].length; i++) {
+			if (CONTRACTOR_INFO[zip][i]["Name"] == contractor["Name"]) {
+				alert("Error: Contractor \""+contractor["Name"]+"\" already present in database. Duplicates cannot be added.");
+				return;
+			}
+		}
+		CONTRACTOR_INFO[zip].push(contractor);
+	} else {
+		CONTRACTOR_INFO[zip] = [contractor];
+	}
 }

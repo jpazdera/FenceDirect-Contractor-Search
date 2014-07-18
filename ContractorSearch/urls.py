8285a0a13ec8511a7e4cd3acdace856f1478776a
Jesse Pazdera <jpazdera@ironwood.haverford.edu>
2014-07-18 18:33:32 -0400

    About to change urls

--- ContractorSearch/urls.py
+++ ContractorSearch/urls.py
@@ -5,9 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', TemplateView.as_view(template_name="index.html")),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^ContractorSearch.html$', TemplateView.as_view(template_name="ContractorSearch.html")),
 
     url(r'^admin/', include(admin.site.urls)),
 )

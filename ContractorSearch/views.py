f3d393fd2d885a9a53e61afee4938a070699d012
jpazdera <jpazdera@haverford.edu>
2014-07-21 15:25:39 -0400

    More small changes

--- ContractorSearch/views.py
+++ ContractorSearch/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 def myView(request):
     return HttpResponse("All set!")
 
-def overwrite_data():
+def overwrite_data(request):
     data = ''
     if request.method == 'POST':
         data = request.POST.get('text', '')
     if data != '':
-        file = open('contractor_info2.js', 'w')
+        file = open('../static/contractor_info2.js', 'w')
         file.write(d)
         file.close
-    return None
+    return render(request, 'index.html')
